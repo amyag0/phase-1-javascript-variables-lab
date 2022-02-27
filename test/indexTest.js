53bd93a90eb1847657afa4c78d11993580efae21
amyag0 <amyago19@gmail.com>
2022-02-26 20:43:17 -0500

    commit tree 424b69da272a2b9d55e5d126256abf9a9ddc8961

--- test/indexTest.js
+++ test/indexTest.js
@@ -7,7 +7,7 @@ describe('index.js', function () {
   describe('companyName', function () {
     it('is set as Scuber', function () {
       expect(companyName).to.equal('Scuber');
-    });
+    });//const companyName="Scuber"
 
     it('is defined as a const', function () {
       expect(js).to.match(/const companyName/, "Expected companyName to be a const");
@@ -17,16 +17,16 @@ describe('index.js', function () {
   describe('mostProfitableNeighborhood', function () {
     it('is declared as equal to Chelsea', function () {
       expect(mostProfitableNeighborhood).to.equal('Chelsea');
-    });
+    }); // let mostProfitableNeighborhood = "Chelsea"
 
     it('is defined using let', function () {
       expect(js).to.match(/let mostProfitableNeighborhood/, "Expected mostProfitableNeighborhood to be defined using let");
-    });
+    }); 
   });
 
   describe('companyCeo', function () {
     it('is declared as equal to Susan Smith', function () {
-      expect(companyCeo).to.equal('Susan Smith');
+      expect(companyCeo).to.equal('Susan Smith'); //let companyCeo="Susan Smith"
     });
 
     it('is defined using let', function () {
